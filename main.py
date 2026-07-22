aebaafb689661021c1e57ff12acbd1a26a450678
Aerospace91 <beenters@gmail.com>
2026-07-22 14:20:06 -0700

    Added Argparse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
@@ -14,12 +15,16 @@ def main():
         api_key=api_key,
     )
     
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    
     messages = [
-        {
-            "role": "user",
-            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-        }
-    ]
+            {
+                "role": "user",
+                "content": args.user_prompt
+            }
+        ]
     
     response = client.chat.completions.create(model="openrouter/free", messages=messages)
     print(f"User prompt: {messages[0]["content"]}")
